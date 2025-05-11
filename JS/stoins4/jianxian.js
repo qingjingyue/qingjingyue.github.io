101cd53fcfa92b8d0639f1420620c8487587786c
清净 <qingjing666666@126.com>
2025-05-12 02:13:02 +0800

    index

--- JS/stoins4/jianxian.js
+++ JS/stoins4/jianxian.js
@@ -1,16 +1,21 @@
-document.addEventListener('scroll', function () {
-    const list = document.getElementById('list');
-    const listRect = list.getBoundingClientRect();
-    const windowHeight = window.innerHeight;
+// 选择所有具有 fade-list 类名的元素
+const lists = document.querySelectorAll('.list');
 
-    if (listRect.top < windowHeight) {
-        const lis = list.getElementsByTagName('li');
-        for (let i = 0; i < lis.length; i++) {
-            setTimeout(() => {
-                lis[i].classList.add('fade-in');
-            }, i * 200);
+// 为每个元素添加滚动事件监听器
+lists.forEach((list) => {
+    document.addEventListener('scroll', function () {
+        const listRect = list.getBoundingClientRect();
+        const windowHeight = window.innerHeight;
+
+        if (listRect.top < windowHeight) {
+            const lis = list.getElementsByTagName('li');
+            for (let i = 0; i < lis.length; i++) {
+                setTimeout(() => {
+                    lis[i].classList.add('fade-in');
+                }, i * 200);
+            }
+            // 移除滚动事件监听器，避免重复触发
+            // document.removeEventListener('scroll', arguments.callee);
         }
-        // 移除滚动事件监听器，避免重复触发
-        document.removeEventListener('scroll', arguments.callee);
-    }
+    });
 });
